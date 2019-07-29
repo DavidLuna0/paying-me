406a36c20e01f47cc443c042c005612828846084
DavidLuna0 <ddavidr569@gmail.com>
2019-07-29 15:51:44 -0300

    Configurando deploy

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 module.exports = {
-  username: 'root',
-  password: 'root',
-  database: 'sequelize',
-  host: '127.0.0.1',
+  username: 'b52e05d5c79e3f',
+  password: '720b09ee',
+  database: 'heroku_954686f24b6f53b',
+  host: 'us-cdbr-iron-east-02.cleardb',
   dialect: 'mysql',
 }
